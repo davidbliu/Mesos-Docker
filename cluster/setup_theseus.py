f5223ec58a0b5cdb5f3abd1c79d825f3c91dc127
david liu <david.liu@autodesk.com>
2014-08-06 11:39:12 -0700

    add cluster materials

--- cluster/setup_theseus.py
+++ cluster/setup_theseus.py
@@ -4,5 +4,5 @@ data = yaml.load(open('config.yaml', 'r'))
 
 mesos_master_host = data['mesos_master_host']
 print 'starting Theseus on this machine...'
-os.system('docker run -d -p 22000:22 -p 5000:5001 -e MARATHON_HOST='+mesos_master_host+' -e MARATHON_PORT=8080 -e ETCD_HOST_ADDRESS='+marathon_master_host+' 54.189.193.228:5000/theseus')
+os.system('docker run -d -p 22000:22 -p 5000:5001 -e MARATHON_HOST='+mesos_master_host+' -e MARATHON_PORT=8080 -e ETCD_HOST_ADDRESS='+mesos_master_host+' 54.189.193.228:5000/theseus')
 print 'started!'
